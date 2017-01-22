391c4be1a370c3681192d4150958b5d1ed79964f
eashish1 <Ashish.Gupta@iiitb.org>
2017-01-22 12:48:07 +0530

    Update Euler80.py

--- HackerRank/Euler/Euler80.py
+++ HackerRank/Euler/Euler80.py
@@ -9,6 +9,5 @@ for i in range(n+1):
     if not sqrt(i)%1 == 0:
         st = str(Decimal(i).sqrt()).replace('.','')[:p]
         digits = map(int,st)
-        #print (i,sum(digits),'-------',sep='\n')
         res += sum(digits)
 print (res)         
